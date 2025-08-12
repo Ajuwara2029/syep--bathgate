537c6e30672e3db2583dd44d6cc5a79fb69c9091
Ajuwara2029 <Ajuwara2029@amsbronx.org>
2025-08-12 14:35:03 +0000

    added css

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-console.log ( " Hello world!");
+const reviews = [
+{
+    id: 1,
+    name: 'Ms. Emmanuel',
+    job:'Humanities Teacher',
+    img: 'IMG_6696.jpeg',
+    text: 'I have been teaching Aisha since 6th grade. Shes a great student with a bright future. She also tends to carry a strong woth ethic. I can not wait to see what the future holds for you!',
+},
 
- let myString="a2b3c";
- console.log (myString);
- 
- let myNumbvar=123;
-console.log(typeof myNumbvar);  
+{
+    id: 2,
+    name: 'Ms. Cole',
+    job:'Middle School Counselor',
+    img: 'cole.webp',
+    text: 'My name is ms. cole  l been a high school counselor in 2009 and l have 2 boys.Aisha is a strong student who manages to push through many challenegs. Shes very outgoing.'
+},
 
-console.log(10 + "eggs");
-console.log(10 +7+"eggs");
-console.log ("eggs" +10 +7);
+{
+    id: 3,
+    name: 'name',
+    job:'peer',
+    img: 'picture',
+    text:'lorem ipsum',
+},
+];
 
-// Both conditions need to be true
-console.log(7>3)&&(5<10);
+//select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-//only one conditions must be true
-console.log (10>3)||(10>12);
+const prevBtn = document.querySelector('.prev-btn'); // make sure HTML uses .prev-btn
+const nextBtn = document.querySelector('.next-btn'); // make sure HTML uses .next-btn
 
-// If the statement is logical or not
-console.log (!5>3); 
+let currentItem = 0;
 
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+    showPerson(currentItem);
+});
 
-function introduction(name,age){
-console.log("Hello, my nameis "+ name + and" I am " + age +"year old. ")
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
 }
 
-introduction("aisha",14)
+// show next person
+nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
 
-const hour = new Date().getHours();
-let greeting= "";
+// show previous person
+prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
 
-if (hour<12){
-greeting="good morning"
-}else if (hour<18){
-  greeting="good afternoon";
-}else{
- greeting="good evening"
-}
 
-document.getElementById("greeting").innerText = greeting;
+
+
+
+
+
+
